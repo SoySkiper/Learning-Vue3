826bf0a4bcd5faabdd4ccb297b19ad53d13c216e
Cristian Primero <soyskiper@gmail.com>
2022-10-08 23:46:08 -0500

    05-rutas, Peticiones HTTP

--- 05-rutas-ciclo/src/modules/shared/components/NavBar.vue
+++ 05-rutas-ciclo/src/modules/shared/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <router-link to="/">Pokemon List</router-link>
-        <router-link to="/id">Pokemon Por Id</router-link>
+        <router-link :to="{name: 'pokemon-id', params: { id: 149}}">Pokemon Por Id</router-link>
         <router-link to="/about">About</router-link>
     </div>    
 </template>
