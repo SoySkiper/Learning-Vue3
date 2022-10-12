c0e88812798ff1eb3eab365fe4bd7b2a6d56f2ba
Cristian Primero <soyskiper@gmail.com>
2022-10-11 22:01:51 -0500

    Router children, layouts

--- 05-rutas-ciclo/src/router/router.js
+++ 05-rutas-ciclo/src/router/router.js
@@ -1,33 +1,65 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
-    { 
-      path: '/', 
-      // Lazy Load
-      component: () => import(/*webpackChunkName: 'ListPages'*/ '../modules/pokemon/pages/ListPages')
-    },
-    { 
-      path: '/about', 
-      component: () => import(/*webpackChunkName: 'AboutPage'*/ '../modules/pokemon/pages/AboutPage') 
-    },
-    { 
-      path: '/:id', 
-      name: 'pokemon-id',
-      component: () => import(/*webpackChunkName: 'id'*/ '../modules/pokemon/pages/PokemonPage'),
-      props: (route) => {
-        const id = Number(route.params.id)
-        return isNaN(id) ? { id:1 } : {id}
-      }
-    },
-    { 
-      path: '/:pathMatch(.*)*', 
-      component: () => import(/*webpackChunkName: 'NoPageFound'*/ "@/modules/shared/pages/NoPagesFound")
-    },
-  ]
+  {
+    path: "/",
+    redirect: "/pokemon",
+  },
+  {
+    path: "/pokemon",
+    name: "pokemon",
+    component: () =>
+      import(
+        /*webpackChunkName: 'PokemonLayout'*/ "../modules/pokemon/layouts/PokemonLayout"
+      ),
+    children: [
+      {
+        path: "home",
+        name: "pokemon-home",
+        // Lazy Load
+        component: () =>
+          import(
+            /*webpackChunkName: 'ListPages'*/ "../modules/pokemon/pages/ListPages"
+          ),
+      },
+      {
+        path: "about",
+        name: "pokemon-about",
+        component: () =>
+          import(
+            /*webpackChunkName: 'AboutPage'*/ "../modules/pokemon/pages/AboutPage"
+          ),
+      },
+      {
+        path: "pokemonid/:id",
+        name: "pokemon-id",
+        component: () =>
+          import(
+            /*webpackChunkName: 'id'*/ "../modules/pokemon/pages/PokemonPage"
+          ),
+        props: (route) => {
+          const id = Number(route.params.id);
+          return isNaN(id) ? { id: 1 } : { id };
+        },
+      },
+      {
+        path: "",
+        redirect: {name: 'pokemon-about'},
+      },
+    ],
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    component: () =>
+      import(
+        /*webpackChunkName: 'NoPageFound'*/ "@/modules/shared/pages/NoPagesFound"
+      ),
+  },
+];
 
-  const router = createRouter({
-    history: createWebHashHistory(),
-    routes, // short for `routes: routes`
-  })
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+});
 
-  export default router
+export default router;
