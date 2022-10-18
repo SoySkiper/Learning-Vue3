b1007d222a66ca9fe5e9d03e768b2c5f641667aa
Cristian Primero <soyskiper@gmail.com>
2022-10-17 23:29:46 -0500

    Vuex, state, mutations, Actions, maps

--- 06-bases-vuex/src/store/index.js
+++ 06-bases-vuex/src/store/index.js
@@ -1,10 +1,34 @@
 import { createStore } from "vuex";
-
+import getRandomInt from "../components/helpers/getRandomInt";
 
 
 export default createStore({
     state: {
         count: 1,
-        lastMutation: 'none'
-    }
+        lastMutation: 'none',
+        isLoading: false,
+        lastRandomInt: 0
+    },
+    mutations: {
+        increment(state) {
+            state.count++
+            state.lastMutation = 'Increment'
+        },
+        incrementBy(state, val) {
+            state.count+= val
+            state.lastMutation = 'IncrementBy ' + val 
+            state.lastRandomInt = val
+        },
+        setLoading() {
+
+        }
+    },
+    // Estas sí pueden ser asíncronas
+    actions: {
+        async incrementRandomInt({ commit }) {
+            const randomInt = await getRandomInt()
+
+            commit('incrementBy', randomInt)
+        },
+    },
 })
